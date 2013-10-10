1c6c66e1d98f3850a51e2264b4c03e3adbeb53ca
fábioandrews <fbioandrews@Andre-pc>
2013-10-10 10:42:28 -0300

    criei uma classe que cria os DAOs e manda para quem precisar apenas a classe abstrata do DAO. No entanto, alguns DAOs não podem ser usados como os abstratos porque possuem metodos muito específicos.

---
+++ MagicBuffetRefatorado/src/entidadesDAO/FabricaDeDAO.java
@@ -0,0 +1,72 @@
+package entidadesDAO;
+
+
+import InterfaceDAO.DAOComBuscaMultiplaInterface;
+import InterfaceDAO.GenericDAOInterface;
+
+public class FabricaDeDAO 
+{
+	public static DAOComBuscaMultiplaInterface criarTemaDAO()
+	{
+		DAOComBuscaMultiplaInterface dao = new TemaDAO();
+		return dao;
+	}
+	
+	public static DAOComBuscaMultiplaInterface criarPacoteDAO()
+	{
+		DAOComBuscaMultiplaInterface dao = new PacoteDAO();
+		return dao;
+	}
+	
+	public static DAOComBuscaMultiplaInterface criarFestaDAO()
+	{
+		DAOComBuscaMultiplaInterface dao = new FestaDAO();
+		return dao;
+	}
+	
+	public static GenericDAOInterface criarPessoaDAO()
+	{
+		GenericDAOInterface dao = new PessoaDAO();
+		return dao;
+	}
+	
+	public static DAOComBuscaMultiplaInterface criarLocalizacaoDAO()
+	{
+		DAOComBuscaMultiplaInterface dao = new LocalizacaoDAO();
+		return dao;
+	}
+	
+	public static GenericDAOInterface criarItemDAO()
+	{
+		GenericDAOInterface dao = new ItemDAO();
+		return dao;
+	}
+	
+	
+	//criacao de DAOs concretos
+	public GerenteDAO criarGerenteDAO()
+	{
+		return new GerenteDAO();
+	}
+	
+	public FestaDAO criarFestaDAOConcreto()
+	{
+		return new FestaDAO();
+	}
+	
+	public ItemDAO criarItemDAOConcreto()
+	{
+		return new ItemDAO();
+	}
+	
+	public PacoteDAO criarPacoteDAOConcreto()
+	{
+		return new PacoteDAO();
+	}
+	
+	public PessoaDAO criarPessoaDAOConcreto()
+	{
+		return new PessoaDAO();
+	}
+	
+}
